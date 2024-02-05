facfd7e8bb21bbff78239d0cbc10a9446d905bbd
cosvmlabs <153391500+cosvmlabs@users.noreply.github.com>
2024-02-06 00:57:18 +0600

    Update index.tsx

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -5,8 +5,8 @@ export default function LoadingPage() {
   return (
     <>
       <Head>
-        <title>Dexplorer</title>
-        <meta name="description" content="Dexplorer" />
+        <title>CVM | SCAN</title>
+        <meta name="description" content="CVM | SCAN" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
