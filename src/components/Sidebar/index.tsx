b833945a201c589f29b1f76211c537ccc181d119
cosvmlabs <153391500+cosvmlabs@users.noreply.github.com>
2024-02-29 04:15:13 +0600

    Update index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -297,7 +297,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
       />
 
       <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-        CVM Explorer
+        EVM Explorer
       </Text>
     </Flex>
   )
