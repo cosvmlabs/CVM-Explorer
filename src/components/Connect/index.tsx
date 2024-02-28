04665d745367b9ec311ac7b48f5abd6833cee6a3
cosvmlabs <153391500+cosvmlabs@users.noreply.github.com>
2024-02-29 04:16:48 +0600

    Update index.tsx

--- src/components/Connect/index.tsx
+++ src/components/Connect/index.tsx
@@ -1,19 +1,42 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const Connect = () => {
-  const router = useRouter();
+  const router = useRouter()
+  const [redirected, setRedirected] = useState(false)
 
   useEffect(() => {
-    // Set item in local storage when component mounts
-    window.localStorage.setItem('https://cvm.cosvm.net', 'https://cvm.cosvm.net');
+    if (typeof document !== 'undefined') {
+      // This code will only run in the browser environment
+      window.localStorage.setItem(
+        'https://cvm.cosvm.net',
+        'https://cvm.cosvm.net'
+      )
+      window.location.reload()
+    }
+  }, [])
 
-    // Redirect to home page
-    router.push('/');
-  }, [router]);
-
-  // Render nothing, as the page should be empty
-  return null;
+  return (
+    <>
+      <svg
+        className="spinner"
+        width="65px"
+        height="65px"
+        viewBox="0 0 66 66"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          className="path"
+          fill="none"
+          strokeWidth="6"
+          strokeLinecap="round"
+          cx="33"
+          cy="33"
+          r="30"
+        ></circle>
+      </svg>
+    </>
+  )
 }
 
-export default Connect;
+export default Connect
